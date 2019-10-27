f065314f133c82b449ccac6e51320508f85fe4dd
Developer <developer@Macs-MacBook-Pro.local>
2019-10-27 19:34:07 +0400

    updated n added more func

--- trie.kt
+++ trie.kt
@@ -8,13 +8,21 @@ class Trie {
 
     private val rootNode: TrieNode = TrieNode()
     
+    fun size(): Int {
+        return wordsCount(rootNode)
+    }
+    
     fun insert(word: String) {
+        if (word.isEmpty())
+        	return
+        
         var current = rootNode
         
-        for(i in 0..word.length-1) {
-            current = current.children.computeIfAbsent(word[i]) {
+        word.forEach {
+            ch -> current = current.children.computeIfAbsent(ch) {
                 _ -> TrieNode()
             }
+
         }
         
         current.endOfWord = true
@@ -24,16 +32,13 @@ class Trie {
 
         var current = rootNode
         
-        if (word.isEmpty() || current.endOfWord || current.children.isEmpty())
-        	return false
-        
-        val stack = Stack<TrieNode>()
+        val stack = Stack<Pair<Char, TrieNode>>()
         
         for (i in 0..word.length - 1) {
             if (!current.children.contains(word[i]))
             	return false
             
-            stack.push(current)
+            stack.push(Pair(word[i], current))
             current = current.children[word[i]]!!
         }
         
@@ -43,72 +48,47 @@ class Trie {
         current.endOfWord = false
         
         if (current.children.isEmpty()) {
-            var popped = stack.pop()
-            
-            while(stack.isNotEmpty() && popped.children.size == 1) {
-                popped.children.clear()
+            while(stack.isNotEmpty()) {
+                val p = stack.pop()
                 
-                if (popped.endOfWord)
+                if (p.second.endOfWord)
                    break
                 
-                popped = stack.pop()
+                p.second.children.remove(p.first)
             }
         }
        
         return true
     }
     
-    fun getAllWords(): ArrayList<String> {
-        val list = arrayListOf<String>()
-        
-        var current: TrieNode = rootNode
-
-        if (!current.endOfWord)
-	        words(current.children, "", list)
-        
-        return list
+    fun getWords(): ArrayList<String> {
+        return words(rootNode, "", arrayListOf<String>())
     }
     
     fun getWords(prefix: String): ArrayList<String> {
-        val list = arrayListOf<String>()
+        val words = arrayListOf<String>()
         
         if (prefix.isNotEmpty()) {
-            var current: TrieNode = rootNode
+            var current = rootNode
             
 	        for (i in 0..prefix.length - 1) {
     	        if (!current.children.contains(prefix[i])) {
-                    return list
+                    return words
             	}
             
 	            current = current.children[prefix[i]]!!
     	    }
             
             if (current.endOfWord)
-            	return arrayListOf<String>(prefix)
+            	words.add(prefix)
                 
-            words(current.children, prefix, list)
+            words(current, prefix, words)
         }
         
-        return list
+        return words
     }
     
-    private fun words(children: HashMap<Char, TrieNode>, prefix: String, words: ArrayList<String>) {
-        for(child in children) {
-            val nstr = prefix + child.key
-            
-            if (child.value.endOfWord)
-               words.add(nstr)
-               
-            if (child.value.children.isNotEmpty())
-            	words(child.value.children, nstr, words)
-        }
-    }
-    
-    fun search(word: String): Boolean {
-        
-        if (word.isEmpty())
-        	return false
-       
+    fun contains(word: String): Boolean {       
         var current = rootNode
         
         for (i in 0..word.length - 1) {
@@ -138,6 +118,29 @@ class Trie {
         
         return s
     }
+     
+    private fun words(node: TrieNode, prefix: String, words: ArrayList<String>): ArrayList<String> {
+        for(child in node.children) {
+            val nstr = prefix + child.key
+            
+            if (child.value.endOfWord)
+               words.add(nstr)
+               
+            words(child.value, nstr, words)
+        }
+        
+        return words
+    }
+    
+    private fun wordsCount(node: TrieNode): Int {
+        var count = if (node.endOfWord) 1 else 0
+        
+        for (child in node.children) {
+            count += wordsCount(child.value)
+        }
+        
+        return count
+    }
 }
 
 
@@ -151,9 +154,9 @@ fun main() {
     t.insert("made")
     t.insert("king")
     t.insert("kong")
-	t.insert("kotlin")
+    t.insert("kotlin")
     t.insert("cake")
-    t.insert("cann")
+    t.insert("cans")
     t.insert("can")
     t.insert("coke")
     t.insert("cane")
@@ -161,16 +164,19 @@ fun main() {
     t.insert("bird")
     t.insert("zigzag")
     
-    println(t.dump())
+    println("dump: " + t.dump())
+
+    println("Total words count: " + t.size())
     
-    println(t.search("can"))
-   	println(t.delete("can"))
-    println(t.search("can"))
-    println(t.search("cann"))
+    println("Contains word 'can': " + t.contains("can"))
+   	println("Delete word 'can': " + t.delete("can"))
+    println("Has word 'can'? " + t.contains("can"))
     
-    println(t.getWords("co").toString())
-    println(t.getWords("ma").toString())
-    println(t.getWords("kotlin").toString())
-    println(t.getAllWords().toString())
-            
+    println("dump: " + t.dump())
+    println("Total words count: " + t.size())
+    println("Print All words: " + t.getWords().toString())
+
+    println("Print Words start with 'co': " + t.getWords("co").toString())
+    println("Print Words start with 'ma': " + t.getWords("ma").toString())
+    println("Print Words start with 'kotlin': " + t.getWords("kotlin").toString())            
 }
